8878311ac04d6792bb49745b176a18796a066347
Cristhian Lopes <dev.cristh.lopes@gmail.com>
2025-12-11 17:47:38 -0300

    feat: Novos serviços na barra lateral

--- frontend/components/nav-main.tsx
+++ frontend/components/nav-main.tsx
@@ -11,6 +11,7 @@ import {
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
 import Link from "next/link";
+import { NewServiceModal } from "./servicos/new-service-modal";
 
 export function NavMain({
   items,
@@ -26,21 +27,23 @@ export function NavMain({
       <SidebarGroupContent className="flex flex-col gap-2">
         <SidebarMenu>
           <SidebarMenuItem className="flex items-center gap-2">
-            <SidebarMenuButton
-              tooltip="Quick Create"
-              className="bg-primary text-primary-foreground hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground min-w-8 duration-200 ease-linear"
-            >
-              <IconCirclePlusFilled />
-              <span>Quick Create</span>
-            </SidebarMenuButton>
-            <Button
+            <NewServiceModal>
+              <SidebarMenuButton
+                tooltip="Novo Serviço"
+                className="bg-primary text-primary-foreground hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground min-w-8 duration-200 ease-linear"
+              >
+                <IconCirclePlusFilled />
+                <span>Novo Serviço</span>
+              </SidebarMenuButton>
+            </NewServiceModal>
+            {/* <Button
               size="icon"
               className="size-8 group-data-[collapsible=icon]:opacity-0"
               variant="outline"
             >
               <IconMail />
               <span className="sr-only">Inbox</span>
-            </Button>
+            </Button> */}
           </SidebarMenuItem>
         </SidebarMenu>
         <SidebarMenu>
